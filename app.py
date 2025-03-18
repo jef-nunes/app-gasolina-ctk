38c0295aa45a8b7acd0543f268046e13dd808893
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-03-18 14:47:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,84 @@
-# Módulos para construção da interface gráfica
 import customtkinter as ctk
 from tkinter import messagebox
+from pathlib import Path
 
-# Definir tema escuro para toda a aplicação
+#---------------------------------------------------------------------------------------------------
+# Personalizar janela da aplicação e barra de titulo
+
 ctk.set_appearance_mode("dark")
 
-# Personalizar janela da aplicação e barra de titulo
 janela = ctk.CTk()
 janela.geometry("600x450")
 janela.resizable(False,False)
 janela.title("App Gasolina 2025")
-janela.iconbitmap("./images/fuelpump.ico")
 
+if Path("./images/fuelpump.ico").exists():
+    janela.iconbitmap("./images/fuelpump.ico")
+#---------------------------------------------------------------------------------------------------
+
+
+
+#---------------------------------------------------------------------------------------------------
+# Função para calcular o resultado de consumo de gasolina
+def calcular_resultado():
+    # Consumo (L)
+    c = int(consumo_entry.get())
+    # Distancia (Km)
+    d = int(distancia_entry.get())
+    # Preço(R$)
+    p = float(preco_entry.get())
+    # Fórmula
+    formula = (d/c)*p
+    # Exibir o resultado
+    messagebox.showinfo("App Gasolina",f"O valor gasto para essa viagem será de R${round(formula, 2)}")
+#---------------------------------------------------------------------------------------------------
+
+
+
+#---------------------------------------------------------------------------------------------------
 # Criar elementos do corpo da janela
 
 # Título
-titulo = ctk.CTkLabel(janela,
+titulo_label = ctk.CTkLabel(janela,
                       text="App Gasolina 2025",
                       text_color="springgreen",
                       font=("verdana",30,"bold"))
 
 # Subtítulo
-subtitulo = ctk.CTkLabel(janela,
+subtitulo_label = ctk.CTkLabel(janela,
                          text="Calcule os seus custos com gasolina",
                          text_color="seagreen",
                          font=("verdana",18,"bold"))
 
 # Input distância percorrida em quilômetros
-distancia = ctk.CTkEntry(janela, placeholder_text="Distância(Km)")
+distancia_entry = ctk.CTkEntry(janela, placeholder_text="Distância(Km)")
 
 # Input consumo em litros
-consumo = ctk.CTkEntry(janela, placeholder_text="Consumo(L)")
+consumo_entry = ctk.CTkEntry(janela, placeholder_text="Consumo(L)")
 
 # Input preço da gasolina em reais
-preco = ctk.CTkEntry(janela, placeholder_text="Preço(R$)")
+preco_entry = ctk.CTkEntry(janela, placeholder_text="Preço(R$)")
 
 # Botão para calcular o resultado
-botao = ctk.CTkButton(janela, text="Calcular")
+calculo_button = ctk.CTkButton(janela, text="Calcular", fg_color="dimgrey", text_color="springgreen", hover_color="dimgrey", command=calcular_resultado)
 
+#---------------------------------------------------------------------------------------------------
+
+
+
+#---------------------------------------------------------------------------------------------------
 # Posicionar elementos
-titulo.place(x=20,y=10)
-subtitulo.place(x=20,y=50)
-distancia.place(x=20,y=110)
-consumo.place(x=20,y=150)
-preco.place(x=20,y=190)
-botao.place(x=20,y=230)
+titulo_label.place(x=20,y=10)
+subtitulo_label.place(x=20,y=50)
+distancia_entry.place(x=20,y=110)
+consumo_entry.place(x=20,y=150)
+preco_entry.place(x=20,y=190)
+calculo_button.place(x=20,y=230)
+#---------------------------------------------------------------------------------------------------
 
+
+
+#---------------------------------------------------------------------------------------------------
 # Iniciar o loop principal da janela
 janela.mainloop()
+#---------------------------------------------------------------------------------------------------
